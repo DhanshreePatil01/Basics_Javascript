e2601a03738fa27aa2322072c1b2990530ec1341
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2023-01-21 00:49:14 +0530

    [Dhanshree]Added :Data types Basics.

---
+++ DataTypes.js
@@ -0,0 +1,44 @@
+// Strings:
+let firstName = "Dhanshree";
+let lastName = "Patil";
+console.log(firstName,lastName);
+
+//number
+let a=52;
+let b=89;
+console.log(a,b);
+
+//Undefined
+let m=undefined;
+console.log(m);
+
+//null
+let n1=null;
+console.log(n1);
+
+// Boolean
+let x = true;
+let y = false;
+console.log(x,y);
+
+// Object:
+const person = {
+    firstName:"Poonam", 
+    lastName:"Mane"
+};
+console.log(person);
+
+// Array object:
+const obj = ["Chetna",10, 20,"Hii"];  //we can store any type of value in array.
+console.log(obj);
+
+// Date object:
+const date = new Date("2000-07-01");
+console.log(date);
+
+//functions
+function square(num)
+{
+    console.log(num*num);
+}
+square(4);
